aaaccf8bd66d961388738fdb0279328eda08c9ef
Stephan <sgriesel@gmail.com>
2020-05-24 14:00:13 +0200

    add app index & icon

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 
+let mainWindow;
+
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'Resize Me',
         width: 400,
         height: 500,
-        autoHideMenuBar: true
+        autoHideMenuBar: true,
+        icon: './assets/icons/icon.png'
     })
+
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 }
 
 app.on('ready', createMainWindow)
